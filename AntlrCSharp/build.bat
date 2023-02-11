8a48687a36bf16d2cf12c65ac148acaae63753ae
GunMetalBlack <powerhousegames206@gmail.com>
2023-02-10 22:26:26 -0500

    Multi-Platform Build Script
    Changed executable publish name to grund and fixed gitignore

---
+++ AntlrCSharp/build.bat
@@ -0,0 +1,4 @@
+dotnet publish -r win-x64 -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -c Release --self-contained true
+dotnet publish -r linux-x64 -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -c Release --self-contained true
+dotnet publish -r osx-x64 -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -c Release --self-contained true
+echo Publish completed!
